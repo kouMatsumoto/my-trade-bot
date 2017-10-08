e09e002e8a7e2ada98e380bcf5287205660352fc
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-10-08 09:19:15 +0900

    feat(api-handler): implement getOrder

--- src/dev.ts
+++ src/dev.ts
@@ -8,27 +8,42 @@ const api = new BitbankApiHandler({
 });
 
 
-console.log(ENV);
 
-const options: BitbankApiOrderOptions = {
-  pair: 'btc_jpy',
-  amount: 0.001,
-  price: 48000,
-  side: 'buy',
-  type: 'limit',
-};
 
-api.createOrder(options).subscribe(
-  (data) => {
-    console.log('data', data);
-  },
-  (error) => {
-    console.error('error', error);
-  },
-  () => {
-    console.log('complete');
-  }
-);
+
+
+function getOrder() {
+  api.getOrder('btc_jpy', '302603751')
+    .subscribe((data) => {
+      console.log(data);
+    });
+}
+
+getOrder();
+
+
+
+function createOrder() {
+  const options: BitbankApiOrderOptions = {
+    pair: 'btc_jpy',
+    amount: 0.001,
+    price: 48000,
+    side: 'buy',
+    type: 'limit',
+  };
+
+  api.createOrder(options).subscribe(
+    (data) => {
+      console.log('data', data);
+    },
+    (error) => {
+      console.error('error', error);
+    },
+    () => {
+      console.log('complete');
+    }
+  );
+}
 
 
 // prevent from exit process.
